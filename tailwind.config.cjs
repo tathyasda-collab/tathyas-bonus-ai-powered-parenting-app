fe7457ea25a0eda428f08d9cde8c912f0c01c573
dostvineet-beep <dostvineet@gmail.com>
2025-10-24 02:44:09 +0530

    Replace Tailwind CDN with compiled Tailwind (PostCSS + tailwind.config); include src/index.css and import in index.tsx

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './**/*.{ts,tsx,js,jsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
